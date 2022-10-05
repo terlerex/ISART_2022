10413b808d65ce06bd5ec00aae0f2accb07b546a
MSI\Maxence <maxence.copchard@gmail.com>
2022-10-05 09:53:06 +0100

    Initial commit

---
+++ Scripts/TitleCard.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public class TitleCard : Control
+{
+
+
+
+    public override void _Ready()
+    {
+
+    }
+
+    void OnPlay()
+    {
+
+    }
+
+    void OnQuit()
+    {
+
+    }
+
+
+}
