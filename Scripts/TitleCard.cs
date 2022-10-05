0494aeb8371e597906de82a64e66f5a7563cc4f5
MSI\Maxence <maxence.copchard@gmail.com>
2022-10-05 10:59:12 +0100

    Commit

--- Scripts/TitleCard.cs
+++ Scripts/TitleCard.cs
@@ -1,25 +1,42 @@
 using Godot;
-using System;
 
-public class TitleCard : Control
+namespace Correction.Scripts
 {
-
-
-
-    public override void _Ready()
+    public class TitleCard : Control
     {
 
+        [Export] private NodePath pathButtonPlay;
+        [Export] private NodePath pathButtonQuit;
+        
+        Button buttonPlay;
+        Button buttonQuit;
+        
+        const string EVENT_BUTTON_UP = "button_up";
+        const string PATH_MAIN_SCENE = "res://Scenes/Main.tscn";
+
+        public override void _Ready()
+        {
+            buttonPlay = GetNode<Button>(pathButtonPlay);
+            buttonQuit = GetNode<Button>(pathButtonQuit);
+            
+            buttonPlay.Connect(EVENT_BUTTON_UP, this, nameof(OnPlay));
+            buttonQuit.Connect(EVENT_BUTTON_UP, this, nameof(OnQuit));
+        }
+
+        void OnPlay()
+        {
+            GetTree().ChangeScene(PATH_MAIN_SCENE);
+        }
+
+        void OnQuit()
+        {
+            GetTree().Quit();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            buttonPlay.Disconnect(EVENT_BUTTON_UP, this, nameof(OnPlay));
+            buttonQuit.Disconnect(EVENT_BUTTON_UP, this, nameof(OnQuit));
+        }
     }
-
-    void OnPlay()
-    {
-
-    }
-
-    void OnQuit()
-    {
-
-    }
-
-
 }
